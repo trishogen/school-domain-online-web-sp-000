58e2b8b2637d0f3da9c22fbe106545fac40efb3b
Trish Ogen <patriciaogen@gmail.com>
2019-05-26 01:01:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,18 @@
 class School
-  attr_reader :name, :roster
+  attr_reader :school_name, :roster
 
 
 
-  def initialize(name)
-    @name = name
+  def initialize(school_name, grade)
+    @school_name = school_name
     @roster = {}
   end
 
-
+  def add_student(name, grade)
+    if not @roster.keys.include? {grade}
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
 
 end
